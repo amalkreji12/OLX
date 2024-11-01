47dee9953b2db20dd5a4ad2355c699e81d84ca8d
Amal K Reji <amalreji45.amal@gmail.com>
2024-11-01 13:41:05 +0530

    login/signup added

--- src/index.js
+++ src/index.js
@@ -3,9 +3,13 @@ import ReactDOM from 'react-dom';
 import App from './App';
 import { FirebaseContext } from './store/FirebaseContext'
 import firebase from './firebase/config'
+import Context from './store/FirebaseContext'
 ReactDOM.render(
-    <FirebaseContext.Provider value={{firebase}}>
-        <App />
+    <FirebaseContext.Provider value={{ firebase }}>
+        <Context>
+            <App />
+        </Context>
+
     </FirebaseContext.Provider>
     , document.getElementById('root'));
 
