a22d26c076553989cb0693451a29d5df6af684e7
Amal K Reji <amalreji45.amal@gmail.com>
2024-11-02 16:38:19 +0530

    post added

--- src/App.js
+++ src/App.js
@@ -1,45 +1,53 @@
-import React,{useEffect,useContext} from 'react';
+import React, { useEffect, useContext } from 'react';
 import './App.css';
 import { BrowserRouter as Router, Route } from 'react-router-dom'
 import Signup from './Pages/Signup';
 import Login from './Pages/Login';
 import Create from './Pages/Create';
-import {AuthContext, FirebaseContext} from './store/FirebaseContext'
+import View from './Pages/ViewPost';
+import { AuthContext, FirebaseContext } from './store/FirebaseContext'
+import Post from './store/PostContext'
 /**
  * ?  =====Import Components=====
  */
 import Home from './Pages/Home';
 
 function App() {
-  const {setUser} = useContext(AuthContext);
-  const {firebase} = useContext(FirebaseContext)
+  const { setUser } = useContext(AuthContext);
+  const { firebase } = useContext(FirebaseContext)
 
-  useEffect(()=>{
-    firebase.auth().onAuthStateChanged((user)=>{
+  useEffect(() => {
+    firebase.auth().onAuthStateChanged((user) => {
       setUser(user);
     })
-    
+
   })
   return (
     <div>
-      <Router>
-        <Route exact path='/'>
-          <Home />
-        </Route>
+      <Post>
+        <Router>
+          <Route exact path='/'>
+            <Home />
+          </Route>
 
-        <Route path='/signup'>
-          <Signup />
-        </Route>
+          <Route path='/signup'>
+            <Signup />
+          </Route>
 
-        <Route path='/login'>
-          <Login/>
-        </Route>
+          <Route path='/login'>
+            <Login />
+          </Route>
 
-        <Route path='/sell'>
-          <Create/>
-        </Route>
+          <Route path='/sell'>
+            <Create />
+          </Route>
 
-      </Router>
+          <Route path='/view'>
+            <View />
+          </Route>
+
+        </Router>
+      </Post>
 
     </div>
   );
